765c26b71e74f267d2baf2746ba5c71a808b78a5
Davi Barbosa Bandeira <daviibandeiraa@gmail.com>
2022-11-16 20:12:55 -0300

    atualizado exercício 07

---
+++ 007/app.js
@@ -0,0 +1,13 @@
+
+function Exibir(){
+
+     let valor1 = document.getElementById('n1').value
+     let valor2 = document.getElementById('n2').value
+     let valor3 = document.getElementById('n3').value
+     let total= valor1
+     let total1 = valor2
+     let total2 = valor3 
+
+
+document.getElementById('resultado').innerHTML = "Número 1: " + total + "<br> Número 2: " + total1 + "<br> Número 3: " + total2
+}
