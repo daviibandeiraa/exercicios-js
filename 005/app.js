11453b6ce27b913a07edc666a8cda6da0d31dcaf
Davi Barbosa Bandeira <daviibandeiraa@gmail.com>
2022-11-16 19:52:39 -0300

    atualizado até o exercicio 06

---
+++ 005/app.js
@@ -0,0 +1,7 @@
+
+function Dividir(){
+    let valor1 = document.getElementById('n1').value
+
+let total= parseFloat(valor1) / parseFloat(100)
+document.getElementById('resultado').innerHTML = total 
+}
